bc0f02d47ab570acdd670c252c6e614d2ed31795
Prathmesh Sinha <77191932+PrathmeshRRDOMIN8@users.noreply.github.com>
2023-03-18 16:30:49 +0530

    Latest Commit

--- lib/constants/constantString.dart
+++ lib/constants/constantString.dart
@@ -1,1 +1,39 @@
 bool showOnboarding = true;
+
+List<String> _communitesNames = [
+    'Business Associations',
+    'Women\'s groups',
+    'Social media groups',
+    'Local Community organisations',
+    'Co-working spaces',
+    'Entrepreneurship programs',
+    'Mentorship programs'
+  ];
+  List<String> _communitiesImages = [
+    'assets/Communities/BusinessAssociation.jpeg',
+    'assets/Communities/WomensGroups.jpeg',
+    'assets/Communities/SocialMedia.jpeg',
+    'assets/Communities/LocalCommunity.jpeg',
+    'assets/Communities/coworkingspace.jpeg',
+    'assets/Communities/EntrepreneurshipProgram.jpeg',
+    'assets/Communities/MentorshipPrograms.jpeg',
+  ];
+  List<String> _communitiesMembers = [
+    '124',
+    '102',
+    '189',
+    '100',
+    '152',
+    '210',
+    '115',
+  ];
+
+  List<String> _commImg = [
+  "assets/CommImg/bsa.jpeg",
+  "assets/CommImg/wg.jpeg",
+  "assets/CommImg/smg.avif",
+  "assets/CommImg/mg.jpeg",
+  "assets/CommImg/cws.jpeg",
+  "assets/CommImg/eg.jpeg",
+  "assets/CommImg/lco.jpeg"
+];
