d3ae7525ad087eca48e35bee3e03565ca308c7f4
Sandeep Kumar Rai <raisandeep.dev@gmail.com>
2023-03-18 16:24:42 +0530

    Updated

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:hack/Community/MainPage.dart';
 import 'package:hack/Courses/Course_Home.dart';
@@ -7,9 +8,23 @@ import 'package:hack/harsh/lib/screen/home/home_page.dart';
 import 'package:hack/pages/home.dart';
 import 'package:provider/provider.dart';
 
+class Friend {
+  final String name;
+  final String imageUrl;
+
+  Friend({required this.name, required this.imageUrl});
+}
+
 class HomePage_Main extends StatefulWidget {
   @override
   _HomePage_MainState createState() => _HomePage_MainState();
+}
+
+class Pair<T1, T2> {
+  final T1 first;
+  final T2 second;
+
+  const Pair(this.first, this.second);
 }
 
 class _HomePage_MainState extends State<HomePage_Main> {
@@ -56,16 +71,130 @@ class _HomePage_MainState extends State<HomePage_Main> {
     }
   }
 
+  final List<Pair<String, int>> features = [
+    Pair('Feature1', 0),
+    Pair('Feature2', 1),
+    Pair('Feature3', 2),
+  ];
+
+  final List<String> activities = [
+    'Activity 1',
+    'Activity 2',
+    'Activity 3',
+    'Activity 4',
+    'Activity 5',
+  ];
+  final List<Friend> friends = [
+    Friend(name: 'John Doe', imageUrl: 'https://picsum.photos/id/237/200/300'),
+    Friend(name: 'Jane Doe', imageUrl: 'https://picsum.photos/id/238/200/300'),
+    Friend(name: 'Bob Smith', imageUrl: 'https://picsum.photos/id/239/200/300'),
+    Friend(
+        name: 'Alice Jones', imageUrl: 'https://picsum.photos/id/240/200/300'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page"),
+        title: Text('Home'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.person),
+            onPressed: () {
+              // TODO: handle profile icon press
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              // TODO: handle settings icon press
+            },
+          ),
+        ],
       ),
-      body: AnimatedContainer(
-        duration: Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-        child: HomeTab(),
+      body: Container(
+        height: 800,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            CarouselSlider(
+              items: features
+                  .map((feature) =>
+                      _buildFeatureCard(feature.first, feature.second))
+                  .toList() as List<Widget>?,
+              options: CarouselOptions(
+                height: 200.0,
+                viewportFraction: 0.8,
+                enableInfiniteScroll: true,
+                autoPlay: true,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Recent Activities',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: activities.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(activities[index]),
+                    subtitle: Text('Today'),
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.red,
+                      child: Text((index + 1).toString()),
+                    ),
+                    onTap: () {
+                      // Open activity detail page
+                    },
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Recent Activities',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+            Expanded(
+                child: ListView.builder(
+              itemCount: friends.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(friends[index].imageUrl),
+                    ),
+                    title: Text(friends[index].name),
+                    trailing: ElevatedButton(
+                      onPressed: () {
+                        // TODO: add friend logic
+                      },
+                      child: Text('Add Friend'),
+                    ),
+                  ),
+                );
+              },
+            ))
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -128,4 +257,61 @@ class SettingsTab extends StatelessWidget {
       child: Text('Settings Tab'),
     );
   }
+}
+
+Widget _buildFeatureCard(String feature, int index) {
+  List<Map<String, String>> items = [
+    {
+      'image': 'https://picsum.photos/id/237/200/300',
+      'title': 'Title 1',
+      'description': 'Description 1'
+    },
+    {
+      'image': 'https://picsum.photos/id/237/200/300',
+      'title': 'Title 2',
+      'description': 'Description 2'
+    },
+    {
+      'image': 'https://picsum.photos/id/237/200/300',
+      'title': 'Title 3',
+      'description': 'Description 3'
+    },
+  ];
+
+  return Container(
+      margin: EdgeInsets.symmetric(horizontal: 4.0),
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      child: Container(
+        child: Column(
+          children: [
+            Container(
+              height: 100,
+              width: 300,
+              child: Image.network(
+                items[index]['image']!,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              items[index]['title']!,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              items[index]['description']!,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey[600],
+              ),
+            ),
+          ],
+        ),
+      ));
 }
